dab7d75508f2454b99d2dfc3afd1e3f67d217480
christians <christians@1086312a-242d-11de-8847-efc87ab67c1b>
2009-04-16 18:56:02 +0000

    - Created Source folder - Added aspect "CheckDisposedAttribute" - Added aspect "DisposableAttribute"
    git-svn-id: http://code.remobjects.com/svn/prismaspects@1 1086312a-242d-11de-8847-efc87ab67c1b

---
+++ Source/Prism.StandardAspects/CheckDisposedAttribute.pas
@@ -0,0 +1,98 @@
+﻿// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+// of the Software, and to permit persons to whom the Software is furnished to do
+// so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+// INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
+// PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+// HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF
+// CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE
+// OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+// This aspect can be added to any method or property. It will insert code, that checks
+// if a field "fIsDisposed" is true and raises an ObjectDisposedException if this is the
+// case.
+// 
+// See also: Prism.StandardAspects.DisposableAttribute
+
+namespace Prism.StandardAspects;
+
+interface
+
+uses 
+  System.Windows.Forms, RemObjects.Oxygene.Cirrus,
+  RemObjects.Oxygene.Cirrus.Values;
+
+type
+  [AttributeUsage(AttributeTargets.Method or AttributeTargets.Property)]
+  CheckDisposedAttribute = public class(Attribute, IMethodImplementationDecorator, IPropertyImplementationDecorator)
+  private
+    method GetField(Services: RemObjects.Oxygene.Cirrus.IServices; aMethod : IMethodDefinition) : IField;
+  public
+    method HandleImplementation(Services: RemObjects.Oxygene.Cirrus.IServices; aMethod: RemObjects.Oxygene.Cirrus.IMethodDefinition);
+    method HandleImplementation(Services: RemObjects.Oxygene.Cirrus.IServices; aProperty: RemObjects.Oxygene.Cirrus.IPropertyDefinition; aRead: RemObjects.Oxygene.Cirrus.IMethodDefinition; aWrite: RemObjects.Oxygene.Cirrus.IMethodDefinition);
+  end;
+  
+implementation
+
+method CheckDisposedAttribute.HandleImplementation(Services: RemObjects.Oxygene.Cirrus.IServices; aMethod: RemObjects.Oxygene.Cirrus.IMethodDefinition);
+begin
+  var theField := GetField(Services, aMethod);
+
+  if theField <> nil then
+  begin
+    var fieldValue := new FieldValue(new SelfValue, theField);
+    var objectName := new DataValue(aMethod.Owner.Name);
+
+    aMethod.SetBody(Services, method begin
+      if unquote<Boolean>(fieldValue) then
+        raise new  ObjectDisposedException(unquote<String>(objectName));
+
+      Aspects.OriginalBody;
+    end);
+  end;
+end;
+
+method CheckDisposedAttribute.HandleImplementation(Services: RemObjects.Oxygene.Cirrus.IServices; aProperty: RemObjects.Oxygene.Cirrus.IPropertyDefinition; aRead: RemObjects.Oxygene.Cirrus.IMethodDefinition; aWrite: RemObjects.Oxygene.Cirrus.IMethodDefinition);
+begin
+  var theField := iif(aRead <> nil, GetField(Services, aRead), GetField(Services, aWrite));
+  var objectName := iif(aRead <> nil, aRead.Owner.Name, aWrite.Owner.Name);
+
+  if theField <> nil then
+  begin
+    var fieldValue := new FieldValue(new SelfValue, theField);
+
+    if aRead <> nil then
+      aRead.SetBody(Services, method begin
+        if unquote<Boolean>(fieldValue) then
+          raise new  ObjectDisposedException(unquote<String>(objectName));
+
+        Aspects.OriginalBody;
+      end);
+
+    if aWrite <> nil then
+      aWrite.SetBody(Services, method begin
+        if unquote<Boolean>(fieldValue) then
+          raise new  ObjectDisposedException(unquote<String>(objectName));
+
+        Aspects.OriginalBody;
+      end);
+  end;
+end;
+
+method CheckDisposedAttribute.GetField(Services: RemObjects.Oxygene.Cirrus.IServices; aMethod : IMethodDefinition) : IField;
+begin
+  result := aMethod.Owner.GetField('fIsDisposed');
+  
+  if result <> nil then
+    if result.Type.Fullname <> 'System.Boolean' then
+      Services.EmitError('fIsDisposed: Field must be of type System.Boolean');
+end;
+
+end.
---
+++ Source/Prism.StandardAspects/Prism.StandardAspects.oxygene
@@ -0,0 +1,71 @@
+<Project DefaultTargets="Build" xmlns="http://schemas.microsoft.com/developer/msbuild/2003" ToolsVersion="3.5">
+  <PropertyGroup>
+    <RootNamespace>Prism.StandardAspects</RootNamespace>
+    <OutputType>Library</OutputType>
+    <AssemblyName>Prism.StandardAspects</AssemblyName>
+    <AllowGlobals>False</AllowGlobals>
+    <AllowLegacyOutParams>False</AllowLegacyOutParams>
+    <AllowLegacyCreate>False</AllowLegacyCreate>
+    <Configuration Condition="'$(Configuration)' == ''">Release</Configuration>
+    <TargetFrameworkVersion>v3.5</TargetFrameworkVersion>
+    <ProjectGuid>{FDFA9E51-2441-4785-9599-44A17D1B72C7}</ProjectGuid>
+  </PropertyGroup>
+  <PropertyGroup Condition=" '$(Configuration)' == 'Debug' ">
+    <DefineConstants>DEBUG;TRACE;</DefineConstants>
+    <OutputPath>.\bin\Debug</OutputPath>
+    <GeneratePDB>True</GeneratePDB>
+    <GenerateMDB>True</GenerateMDB>
+  </PropertyGroup>
+  <PropertyGroup Condition=" '$(Configuration)' == 'Release' ">
+    <OutputPath>.\bin\Release</OutputPath>
+    <EnableAsserts>False</EnableAsserts>
+  </PropertyGroup>
+  <ItemGroup>
+    <Reference Include="mscorlib">
+      <HintPath>$(Framework)\mscorlib.dll</HintPath>
+    </Reference>
+    <Reference Include="RemObjects.Oxygene.Cirrus">
+      <HintPath>$(ProgramFiles)\CodeGear\Delphi Prism\Bin\RemObjects.Oxygene.Cirrus.dll</HintPath>
+      <Name>RemObjects.Oxygene.Cirrus.dll</Name>
+      <Private>True</Private>
+    </Reference>
+    <Reference Include="System">
+      <HintPath>$(Framework)\System.dll</HintPath>
+    </Reference>
+    <Reference Include="System.Core">
+      <HintPath>$(ProgramFiles)\Reference Assemblies\Microsoft\Framework\v3.5\System.Core.dll</HintPath>
+    </Reference>
+    <Reference Include="System.Data">
+      <HintPath>$(Framework)\System.Data.dll</HintPath>
+    </Reference>
+    <Reference Include="System.Data.DataSetExtensions">
+      <HintPath>$(ProgramFiles)\Reference Assemblies\Microsoft\Framework\v3.5\System.Data.DataSetExtensions.dll</HintPath>
+    </Reference>
+    <Reference Include="System.Drawing">
+      <HintPath>$(Framework)\System.Drawing.dll</HintPath>
+    </Reference>
+    <Reference Include="System.Windows.Forms">
+      <HintPath>$(Framework)\System.Windows.Forms.dll</HintPath>
+    </Reference>
+    <Reference Include="System.Xml">
+      <HintPath>$(Framework)\System.Xml.dll</HintPath>
+    </Reference>
+    <Reference Include="System.Xml.Linq">
+      <HintPath>$(ProgramFiles)\Reference Assemblies\Microsoft\Framework\v3.5\System.Xml.Linq.dll</HintPath>
+    </Reference>
+  </ItemGroup>
+  <ItemGroup>
+    <Compile Include="CheckDisposedAttribute.pas" />
+    <Compile Include="DisposableAttribute.pas" />
+    <Compile Include="Properties\AssemblyInfo.pas" />
+    <EmbeddedResource Include="Properties\Resources.resx">
+      <Generator>ResXFileCodeGenerator</Generator>
+    </EmbeddedResource>
+    <Compile Include="Properties\Resources.Designer.pas" />
+    <None Include="Properties\Settings.settings">
+      <Generator>SettingsSingleFileGenerator</Generator>
+    </None>
+    <Compile Include="Properties\Settings.Designer.pas" />
+  </ItemGroup>
+  <Import Project="$(MSBuildExtensionsPath)\RemObjects Software\Oxygene\RemObjects.Oxygene.targets" />
+</Project>
