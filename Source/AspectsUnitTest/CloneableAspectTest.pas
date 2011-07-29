6af17687fc817e6a435d80395505c5f9d1f4db06
jeroen <jeroen@1086312a-242d-11de-8847-efc87ab67c1b>
2011-07-29 15:19:21 +0000

    added unittest for Cloneable Aspect
    git-svn-id: http://code.remobjects.com/svn/prismaspects@43 1086312a-242d-11de-8847-efc87ab67c1b

---
+++ Source/AspectsUnitTest/CloneableAspectTest.pas
@@ -0,0 +1,42 @@
+﻿namespace AspectsUnitTest;
+
+interface
+
+uses
+  Prism.StandardAspects,
+  NUnit.Framework;
+
+type
+  [TestFixture]
+  CloneableAspectTest = public class
+  private
+  protected
+  public
+    [Test]
+    method TestClone;
+  end;
+
+  [Aspect:Cloneable]
+  Class2 = public class
+  private
+  public
+    property Test1: Int32;
+    property Test2: String;
+    property Test3: Boolean;
+  end;
+
+implementation
+
+method CloneableAspectTest.TestClone;
+begin
+  var a := new Class2;
+  a.Test1 := 55;
+  a.Test2 := 'Test';
+  a.Test3 := true;
+  var b := a.Clone as Class2;
+  Assert.AreEqual(a.Test1, b.Test1);
+  Assert.AreEqual(a.Test2, b.Test2);
+  Assert.AreEqual(a.Test3, b.Test3);
+end;
+
+end.
